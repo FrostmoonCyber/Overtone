88f8af7cff4c331de77d7b28e658fe0edbf0aa6a
FrostmoonCyber <lilianacastrolomba@gmail.com>
2026-08-13 19:44:59 +0200

    Update Initial Configuration

---
+++ overtone/__init__.py
@@ -0,0 +1,8 @@
+# ==============================================================================
+# Overtone
+# Copyright (c) 2026 FrostmoonCyber. All Rights Reserved.
+# Author: FrostmoonCyber
+# Repository: https://github.com/FrostmoonCyber/AETHEL-Engine
+# 
+# Licensed under PolyForm Noncommercial License 1.0.0.
+# ==============================================================================
